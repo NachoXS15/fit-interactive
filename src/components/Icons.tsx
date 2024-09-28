871a1d73bd10cbd6107d8345b746845003c199f2
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-09-28 07:49:48 -0300

    finished 'map', uploading images

---
+++ src/components/Icons.tsx
@@ -0,0 +1,23 @@
+export function ArrowBack() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="white" className="size-12">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m11.25 9-3 3m0 0 3 3m-3-3h7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+        </svg>
+
+    )
+}
+export function LeftArrowIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+        </svg>
+
+    )
+}
+export function RightArrowIcon() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
+        </svg>
+    )
+}
---
+++ src/config/MapInfo.ts
@@ -0,0 +1,194 @@
+const MapInfo = [
+    {
+        "id": 1,
+        "nameDep": "Arauco",
+        "desc": "Posee una diversidad de atractivos, entre los que podemos destacar el Olivo Cuatricentenario (el olivo más antiguo del país, declarado monumento histórico nacional); las Termas de Santa Teresita (sus aguas termales ofrecen propiedades curativas en el marco de un paisaje único y privilegiado); el barreal de Arauco, Complejo Vientos del Señor (donde se pueden realizar caminatas, andar en bici y practicar carrovelismo o kitebuggy). Visitar y contemplar al Señor de la Peña. También cabe destacar que en este departamento se realiza la Fiesta Nacional de la Olivicultura cada 24 de mayo en Aimogasta.",
+        "img": [
+            "/assets/map/deptos/arauco/1.jpg",
+            "/assets/map/deptos/arauco/2.jpg",
+            "/assets/map/deptos/arauco/3.jpg",
+            "/assets/map/deptos/arauco/4.jpg",
+            "/assets/map/deptos/arauco/5.jpg",
+            "/assets/map/deptos/arauco/6.jpg",
+            "/assets/map/deptos/arauco/7.jpeg",
+            "/assets/map/deptos/arauco/8.jpeg",
+            "/assets/map/deptos/arauco/9.jpeg",
+            "/assets/map/deptos/arauco/10.jpg",
+            "/assets/map/deptos/arauco/11.jpg",
+            "/assets/map/deptos/arauco/12.JPG",
+            "/assets/map/deptos/arauco/13.jpg",
+        ],
+        "region": "Norte"
+    },
+    {
+        id: 2,
+        nameDep: "Capital",
+        desc: "La ciudad de La Rioja cuenta con numerosos atractivos para conocer donde podemos mencionar museos sobre los orígenes, cultura, historia y progreso de la provincia; iglesias, el mercado artesanal, el Memorial de Juan Facundo Quiroga, el Paseo Cultural Pedro Ignacio de Castro Barros, Paseo de los Sauces donde se puede practicar kayak, Reserva Municipal Juan Caro. El Circuito Religioso en el casco histórico es uno de los recorridos principales de la provincia. También es famosa por actividades de vuelo libre, como aladelta y parapente en el Cerro El Morro.",
+        img: [
+             "/assets/map/deptos/capital/1.JPG",
+             "/assets/map/deptos/capital/2.JPG",
+             "/assets/map/deptos/capital/3.jpg",
+             "/assets/map/deptos/capital/4.jpg",
+             "/assets/map/deptos/capital/5.jpg",
+             "/assets/map/deptos/capital/6.jpg",
+             "/assets/map/deptos/capital/7.jpg",
+             "/assets/map/deptos/capital/8.jpg",
+             "/assets/map/deptos/capital/9.JPG",
+             "/assets/map/deptos/capital/10.jpg",
+             "/assets/map/deptos/capital/11.jpg",
+             "/assets/map/deptos/capital/12.jpg",
+             "/assets/map/deptos/capital/13.jpg",
+             "/assets/map/deptos/capital/14.jpg",
+             "/assets/map/deptos/capital/15.jpg",
+             "/assets/map/deptos/capital/16.JPG",
+             "/assets/map/deptos/capital/17.jpg",
+             "/assets/map/deptos/capital/18.jpg",
+        ],
+        region: "Norte"
+    },
+    {
+        id: 3,
+        nameDep: "Castro Barros",
+        desc: "Conocido popularmente como la 'Costa Riojana', compuesto por pequeños poblados y caracterizado por una paz y tranquilidad inigualable, invita al turista a estar en contacto con la naturaleza. Ofrece recorridos por capillas, artesanos y productos regionales. Actividades como trekking, excursiones entre quebradas, y la visita a bodegas artesanales enriquecen la experiencia.",
+        img: [
+            "/assets/map/deptos/castro_barros/1.jpg",
+             "/assets/map/deptos/castro_barros/2.jpg",
+             "/assets/map/deptos/castro_barros/3.jpg",
+             "/assets/map/deptos/castro_barros/4.jpeg",
+             "/assets/map/deptos/castro_barros/5.jpeg",
+             "/assets/map/deptos/castro_barros/6.jpeg",
+             "/assets/map/deptos/castro_barros/7.jpg",
+             "/assets/map/deptos/castro_barros/8.jpg",
+             "/assets/map/deptos/castro_barros/9.jpg",
+             "/assets/map/deptos/castro_barros/10.jpg",
+             "/assets/map/deptos/castro_barros/11.jpg",
+             "/assets/map/deptos/castro_barros/12.jpg",
+             "/assets/map/deptos/castro_barros/13.jpg",
+             "/assets/map/deptos/castro_barros/14.jpg",
+             "/assets/map/deptos/castro_barros/15.JPG",
+             "/assets/map/deptos/castro_barros/16.jpg",
+             "/assets/map/deptos/castro_barros/17.jpg",
+             "/assets/map/deptos/castro_barros/18.jpg",
+             "/assets/map/deptos/castro_barros/19.jpg",
+        ],
+        region: "Norte"
+    },
+    {
+        id: 4,
+        nameDep: "Chamical",
+        desc: "Este departamento ubicado en la Costa Baja de Los Llanos Riojanos es cuna de festividades religiosas como Nuestra Señora del Rosario de San Nicolás y Santa Lucía. Entre sus atractivos destacan el Dique La Aguadita, perfecto para actividades recreativas al aire libre, y la Posta Turística Kakan, un centro de interpretación que enriquece la visita.",
+        img: [
+            "/assets/map/deptos/chamical/1.jpg",
+            "/assets/map/deptos/chamical/2.jpg",
+            "/assets/map/deptos/chamical/3.jpg",
+            "/assets/map/deptos/chamical/4.jpg",
+            "/assets/map/deptos/chamical/5.jpg",
+            "/assets/map/deptos/chamical/6.jpg",
+            "/assets/map/deptos/chamical/7.jpg",
+            "/assets/map/deptos/chamical/8.jpg",
+            "/assets/map/deptos/chamical/9.JPG",
+            "/assets/map/deptos/chamical/10.JPG",
+        ],
+        region: "Sur"
+    },
+    {
+        id: 5,
+        nameDep: "Chilecito",
+        desc: "Es la segunda ciudad más importante de la provincia de La Rioja. Rodeada por los cordones del Velazco y la Sierra de Famatina, Chilecito ofrece atractivos como el Cable Carril (Monumento Histórico Nacional), la Mina La Mejicana y los senderos del Famatina. Forma parte de la Ruta del Vino, con bodegas que permiten degustar vinos locales.",
+        img: [],
+        region: "Oeste"
+    },
+    {
+        id: 6,
+        nameDep: "General Felipe Varela",
+        desc: "Este departamento alberga atractivos turísticos como el Parque Nacional Talampaya (una de las 7 maravillas de Argentina y Patrimonio de la Humanidad), Vallecito Encantado, Cañón del Triásico y Cañón de Anchumbil. Es un destino ideal para el turismo todo el año, destacando también sitios como Aicuña y otras maravillas naturales.",
+        img: [],
+        region: "Oeste"
+    },
+    {
+        id: 7,
+        nameDep: "Famatina",
+        desc: "Famatina es un área rica en flora y fauna, con atractivos como el Cerro Famatina, Ruinas de la Mina del Oro, y el Cañón del Ocre. El turismo de aventura, senderismo y escalada son las principales actividades, destacando la conexión con la naturaleza y las tradiciones locales.",
+        img: [],
+        region: "Oeste"
+    },
+    {
+        id: 8,
+        nameDep: "Gral. Ángel V. Peñaloza",
+        desc: "Este pintoresco departamento destaca por la Quebrada de los Cóndores, donde es posible observar una gran cantidad de ejemplares de cóndor andino. Otros atractivos son la casa natal de Abel Bazán y Bustos, la Iglesia Nuestra Señora del Rosario de Tama y la Sierra de los Quinteros.",
+        img: [],
+        region: "Sur"
+    },
+    {
+        id: 9,
+        nameDep: "Gral. Belgrano",
+        desc: "Este departamento ofrece sitios de interés como la Parroquia Nuestra Señora de Candelaria, Capilla Virgen de Fátima, Cristo de los Llanos, el Dique de Olta y el Bosque Petrificado de Olta. Es un destino ideal para el turismo cultural y recreativo.",
+        img: [],
+        region: "Sur"
+    },
+    {
+        id: 10,
+        nameDep: "Gral. Juan Facundo Quiroga",
+        desc: "Con recorridos históricos vinculados a las luchas federales y culturas originarias, este departamento es ideal para el turismo religioso e histórico. Entre sus principales atractivos están la Ruta de los Caudillos, la Reserva Provincial Guasamayo y la Iglesia Nuestra Señora de Copacabana.",
+        img: [],
+        region: "Sur"
+    },
+    {
+        id: 11,
+        nameDep: "Gral. Lamadrid",
+        desc: "Este departamento es famoso por el turismo aventura, especialmente por sus paisajes imponentes, escenario del Rally Dakar. Entre sus atractivos están la Quebrada del Yeso, Senderos de Peregrinos, El Infiernillo y el Cerro El Toro.",
+        img: [],
+        region: "Oeste"
+    },
+    {
+        id: 12,
+        nameDep: "Gral. Ortiz de Ocampo",
+        desc: "Con atractivos como las aguas termales de Ambil y la Cruz Gloriosa (Dozulé), este departamento combina turismo natural y religioso. La flora y fauna peculiar de la región, junto con sitios históricos, hacen de este un destino con encanto.",
+        img: [],
+        region: "Sur"
+    },
+    {
+        id: 13,
+        nameDep: "Gral. San Martín",
+        desc: "Este departamento ofrece un turismo basado en la historia, la naturaleza y la religión. Entre sus atractivos destacan Pampa de las Salinas, Cuatro Esquinas, y diversos senderos agrestes ideales para caminatas y cabalgatas.",
+        img: [],
+        region: "Sur"
+    },
+    {
+        id: 14,
+        nameDep: "Independencia",
+        desc: "Es un departamento con un gran potencial turístico gracias a sus atractivos naturales como la Reserva Provincial de Usos Múltiples Los Colorados, la Cueva del Chacho y el Parque Provincial El Chiflón. Es ideal para los amantes de la naturaleza y el turismo aventura.",
+        img: [],
+        region: "Sur"
+    },
+    {
+        id: 15,
+        nameDep: "Rosario V. Peñaloza",
+        desc: "Único departamento con nombre de mujer en honor a la Maestra de la Patria, ofrece un novedoso circuito de Turismo Religioso, tanto urbano como rural. Algunos de sus principales atractivos son el Cerro de la Cruz, la Iglesia Inmaculada Concepción de Chepes y el Museo de los Caudillos.",
+        img: [],
+        region: "Sur"
+    },
+    {
+        id: 16,
+        nameDep: "San Blas de los Sauces",
+        desc: "Este pintoresco departamento es famoso por el Sitio Arqueológico de Hualco y la Cascada Cola de la Novia. Sus pequeñas localidades brindan una cálida hospitalidad, ofreciendo un ambiente acogedor y relajante para los turistas.",
+        img: [],
+        region: "Norte"
+    },
+    {
+        id: 17,
+        nameDep: "Sanagasta",
+        desc: "Con un microclima especial, Sanagasta es un oasis en verano. Entre sus principales atractivos se encuentran el Mercado Artesanal, la Bodega Loma Blanca y la Cueva de las Brujas, además del popular Festival Chayero Sanagasteño y el Parque Geológico de Dinosaurios.",
+        img: [],
+        region: "Norte"
+    },
+    {
+        id: 18,
+        nameDep: "Vinchina",
+        desc: "Este departamento se destaca por sus increíbles paisajes naturales como la Reserva Provincial Laguna Brava, el Cráter Corona del Inca y la Quebrada de La Troya. Es un punto estratégico para el turismo de aventura, con rutas desafiantes y vistas imponentes.",
+        img: [],
+        region: "Oeste"
+    }
+]
+
+export default MapInfo
